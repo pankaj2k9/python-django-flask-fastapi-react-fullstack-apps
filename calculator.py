66ee3e201c7e3a9641c456cfa66a4a9c0af04d1d
pankaj2k9 <pkp2.me2k9@gmail.com>
2024-11-07 23:59:06 +0600

    added calculator project code

---
+++ calculator.py
@@ -0,0 +1,31 @@
+print("Welcome to Calculator Project")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+
+option = int(input("Select an option for Basic Calculator Operation: "))
+
+if option == 1:
+    n1 = int(input("Enter 1st Number: "))
+    n2 = int(input("Enter 2nd Number: "))
+    n3 = n1 + n2
+    print("Addition is: " + str(n3))
+elif option == 2:
+    n1 = int(input("Enter 1st Number: "))
+    n2 = int(input("Enter 2nd Number: "))
+    n3 = n1 - n2
+    print("Subtraction is: " + str(n3))
+elif option == 3:
+    n1 = int(input("Enter 1st Number: "))
+    n2 = int(input("Enter 2nd Number: "))
+    n3 = n1 * n2
+    print("Multiplication is: " + str(n3))
+elif option == 4:
+    n1 = int(input("Enter 1st Number: "))
+    n2 = int(input("Enter 2nd Number: "))
+    n3 = n1 / n2
+    print("Division is: " + str(n3))
+else:
+    print("Invalid Input")
